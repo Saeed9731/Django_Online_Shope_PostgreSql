1ea3bb036ff8279f33ef02700bd42b7d155e9b46
Saeed Physics <sdmi13790@gmail.com>
2022-09-14 19:38:33 +0430

    add static files

--- config/settings.py
+++ config/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
-from tkinter import E
 from environs import Env
+import os
 
 # for environments variables
 env = Env()
@@ -140,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
