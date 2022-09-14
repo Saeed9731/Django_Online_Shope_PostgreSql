063482d6f8a86e97c71822b1ecb4d265f7dbf1e5
Saeed Physics <sdmi13790@gmail.com>
2022-09-14 21:30:10 +0430

    add comment models

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 
 class Product(models.Model):
@@ -17,3 +18,23 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('Detail', args=[self.pk])
+
+
+class Comment(models.Model):
+    PRODUCT_STARS = [
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
+    body = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    active = models.BooleanField(default=True)
