d963a1f8312abe812680426dcd337781bf9f8220
Saeed Physics <sdmi13790@gmail.com>
2023-07-04 17:13:49 +0330

    Verbose name

--- products/models.py
+++ products/models.py
@@ -33,9 +33,14 @@ class Comment(models.Model):
     ]
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', )
-    body = models.TextField()
-    stars = models.CharField(max_length=10, choices=PRODUCT_STARS)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='comments author',
+    )
+    body = models.TextField(verbose_name='')
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name='')
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now_add=True)
