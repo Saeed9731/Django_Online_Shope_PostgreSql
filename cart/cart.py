1f2e485013f0285a548a062aa7eb406c119384cb
Saeed Physics <sdmi13790@gmail.com>
2023-07-07 10:31:44 +0330

    adding product to cart

---
+++ cart/cart.py
@@ -0,0 +1,31 @@
+class Cart:
+    def __int__(self, request):
+        """
+        Initialize the cart
+        """
+        self.request = request
+        self.session = request.session
+
+        cart = self.session.get('cart')
+        if not cart:
+            cart = self.session['cart'] = {}
+
+        self.cart = cart
+
+    def add(self, product, quantity=1):
+        """
+        Add the specified product to the cart if it exits
+        """
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': quantity}
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        """
+        Mark session ad modified to save changes
+        """
+        self.session.modified = True
