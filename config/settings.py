59ae7096bba66a4e15f6f164a226a40a38bffc29
Saeed Physics <sdmi13790@gmail.com>
2023-07-18 18:07:27 +0330

    convert english number to persian

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     'pages',
     'products.apps.ProductsConfig',
     'cart',
+    'persian_tr',
 ]
 
 SITE_ID = 1
---
+++ persian_tr/templatetags/persian_translation_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def translate_numbers(value):
+    value = str(value)
+    english_to_persian_table = value.maketrans('0123456789', '۰١٢٣٤٥٦٧٨٩')
+    return value.translate(english_to_persian_table)
