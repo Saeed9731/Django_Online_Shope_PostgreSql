604f5de39b2a5ac5c9808358200f98ab86b45804
Saeed Physics <sdmi13790@gmail.com>
2023-07-18 14:59:45 +0330

    add media for products

--- config/settings.py
+++ config/settings.py
@@ -156,6 +156,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.join(BASE_DIR,'media/'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import  settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,4 @@ urlpatterns = [
 
     # Roseta (i18n)
     path('rosetta/', include('rosetta.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
